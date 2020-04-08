837723ad023de7cd200ee3ee1274ce593bdc33ec
Ygor <ygor_vinicius@hotmail.com>
2020-04-08 20:51:07 -0300

    Desafio 03 - Conceitos de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Repositório Ruby on Rails", 
+ 	    url: "http://github.com/...", 
+	    techs: ["Node.js"]
+    });
+    setRepositories([ ...repositories, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+    repositories.splice(repositorieIndex, 1);
+    setRepositories([ ...repositories ]);
   }
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repositorie => <li key={repositorie.id}>
+          {repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
